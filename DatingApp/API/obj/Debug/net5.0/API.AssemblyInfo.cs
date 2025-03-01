5efed5d85ee7cf8260072e18eec80a937b0bb0b6
Ridwan Galib <galib266@gmail.com>
2025-03-01 20:01:13 +0600

    End of class 015

--- DatingApp/API/obj/Debug/net5.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net5.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c918256b482f767a8c4227bb5fef365c64aeed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4052fab464050391864ab7f0563d6a06681ddc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
