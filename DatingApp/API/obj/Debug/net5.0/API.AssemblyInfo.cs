fe17e6bbbef17ef8c0400d67e1eb7679d871e19c
Ridwan Galib <galib266@gmail.com>
2025-03-01 20:29:06 +0600

    ok

--- DatingApp/API/obj/Debug/net5.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net5.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a6f845c60f560780b228da05a590e652403213c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dfeedb5387127a55b4c25b0f1c129557c65bfc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
