000b955018b608facdca543c1bad86f2bcf5eaf7
WorkShop\Galib <galib9559@gmail.com>
2025-04-14 15:59:48 +0600

    End of session 3

--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  users:any;
+
+  constructor(private http :HttpClient){
+
+  }
+  ngOnInit() {
+    this.getUser();
+  }
+  getUser(){
+    this.http.get('http://localhost:34815/api/Users').subscribe(response=>{
+      this.users=response
+    },
+    error=>{
+      console.log(error);
+    }
+  );
+  }
 }
