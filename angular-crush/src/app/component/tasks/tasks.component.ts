e8605d158bf554b611974d8ac87721d42652a0a2
WorkShop\Galib <galib9559@gmail.com>
2025-05-01 22:35:38 +0600

    post crush course

--- angular-crush/src/app/component/tasks/tasks.component.ts
+++ angular-crush/src/app/component/tasks/tasks.component.ts
@@ -9,12 +9,25 @@ import { Task } from 'src/app/Task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
+
   tasks:Task[]=[];
 
   constructor(private taskService:TaskService) { }
 
   ngOnInit(): void {
-    this.tasks=this.taskService.getTask();
+    this.taskService.getTask().subscribe((response)=>
+    (this.tasks=response));
   }
-
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe((response)=>
+      (this.tasks=this.tasks.filter(t=>t.id!==task.id)))
+    }
+  toggleReminder(task: Task) {
+      task.reminder=!task.reminder;
+      this.taskService.updateTaskReminder(task).subscribe();
+  }
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((response:Task)=>
+      (this.tasks.push(response)));
+  }
 }
